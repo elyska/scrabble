08b7ea92e4d4c5b0bc4fc53a0e45cda52e7707f0
Eliska Ryklova <ryklovae@uni.coventry.ac.uk>
2023-05-06 12:37:51 +0100

    Styling

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,25 +2,29 @@
 
 @section('content')
 <div class="container">
-    <h1>{{ __('New Game') }}</h1>
-    <form action="{{ route('new-game') }}" method="post">
-        @csrf
-        <p class="mb-3">
-            <label for="opponent" class="form-label">{{ __('Opponent Name') }}</label>
-            <input class="form-control" type="text" name="opponent">
-            @error('opponent')
+    <div class="row justify-content-center">
+        <div class="col-12 col-md-5 col-lg-3">
+            <h1 class="mb-4 text-center">{{ __('New Game') }}</h1>
+
+            <form action="{{ route('new-game') }}" method="post">
+                @csrf
+                <p class="mb-4">
+                    <input class="form-control" type="text" name="opponent" placeholder="{{ __('Opponent Name') }}">
+                @error('opponent')
                 <div class="alert alert-danger">{{ $message }}</div>
-            @enderror
-        </p>
+                @enderror
+                </p>
 
-        <p class="mb-3">
-            <label for="language" class="form-label">{{ __('Language') }}</label>
-            <select class="form-control"  name="language">
-                <option value="CS">{{ __("Czech") }}</option>
-            </select>
-        </p>
+                <p class="mb-4">
+                    <select class="form-control"  name="language">
+                        <option value="CS" disabled selected>{{ __("Language") }}</option>
+                        <option value="CS">{{ __("Czech") }}</option>
+                    </select>
+                </p>
 
-        <button class="btn btn-primary" type="submit">{{ __("Create Game") }}</button>
-    </form>
+                <button class="btn w-100" type="submit">{{ __("Create Game") }}</button>
+            </form>
+        </div>
+    </div>
 </div>
 @endsection
